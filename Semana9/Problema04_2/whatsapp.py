cfce91e51fd2393372e4a4d9609ea82c979fe18f
pedrohrpc <pedrohrpcorrea@gmail.com>
2023-12-04 19:12:20 -0300

    Exercícios semana 9 e 10

---
+++ Semana9/Problema04_2/whatsapp.py
@@ -0,0 +1,17 @@
+
+#teste
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+
+class Whats1App(App):
+    pass
+
+if __name__ == "__main__":
+    Whats1App().run()
+    
